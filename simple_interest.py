ebc6e3fca68a3cfcd7e521a1a529c50147102684
Wilfred Osozi <osoziw@gmail.com>
2025-11-04 21:11:58 +0300

    added a simple interest script

---
+++ simple_interest.py
@@ -0,0 +1,13 @@
+ 
+# (I = P * R * T)
+# principal with a value of 1000 (representing $1000),
+# rate with a value of 0.05 (representing 5% annual interest rate),
+# time with a value of 3 (representing 3 years).
+
+principal = 1000
+rate = 0.05
+time = 3
+
+interest = (principal * rate * time)
+
+print("The simple interest is:", interest)
