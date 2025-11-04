1ca17829edd9e44904aec9403da12556f72db3f6
Wilfred Osozi <osoziw@gmail.com>
2025-11-04 21:27:24 +0300

    updated

--- simple_interest.py
+++ simple_interest.py
@@ -1,13 +1,5 @@
- 
-# (I = P * R * T)
-# principal with a value of 1000 (representing $1000),
-# rate with a value of 0.05 (representing 5% annual interest rate),
-# time with a value of 3 (representing 3 years).
-
 principal = 1000
 rate = 0.05
 time = 3
-
 interest = principal * rate * time
-
 print(interest)
