e59ec33e6bc8e7bd5540c09524cadc03b60558c8
Wilfred Osozi <osoziw@gmail.com>
2025-11-16 17:52:47 +0300

    add range

--- control-flow/multiplication_table.py
+++ control-flow/multiplication_table.py
@@ -1,5 +1,5 @@
-numbers = [1,2,3,4,5,6,7,8,9,10]
+
 number = int(input("Enter a number to see its multiplication table: ")) 
-for num in numbers:
+for num in range(1, 11):
     product = num *number
     print(number, "x", num, "=", product)
