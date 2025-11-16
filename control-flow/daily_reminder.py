a67bb0d32905abf9ab9abde4e6b4e8d87908c8b5
Wilfred Osozi <osoziw@gmail.com>
2025-11-16 21:47:56 +0300

    updated the script

--- control-flow/daily_reminder.py
+++ control-flow/daily_reminder.py
@@ -4,26 +4,22 @@ time_bound = input("Is it time-bound? (yes/no): ").lower()
 
 match priority:
     case "high":
-        reminder = f"Reminder: '{task}' is a high priority task"
         if time_bound == "yes":
-            reminder += " that requires immediate attention today!"
+            print(f"Reminder: '{task}' is a {priority} priority task that requires immediate attention today!")
         else:
-            reminder += ". Try to complete it soon."
-    
-    case "medium":
-        reminder = f"Reminder: {task} is a {priority} priority task"
-        if time_bound == "yes":
-            reminder += " that should be addressed today."
-        else:
-            reminder += ". Consider doing it when convenient."
-    
-    case "low":
-        reminder = f"Note: '{task}' is a {priority} priority task"
-        if time_bound == "no":
-            reminder += " that you may want to finish today"
-        else:
-            reminder += ". Consider completing it when you have free time."
-    case _:
-        reminder = "Invalid priority entered. Please use high, medium, or low"
+            print(f"Reminder: '{task}' is a {priority} priority task. Try to complete it soon.")
 
-print(reminder)
+    case "medium":
+        if time_bound == "yes":
+            print(f"Reminder: '{task}' is a {priority} priority task that should be addressed today.")
+        else:
+            print(f"Reminder: '{task}' is a {priority} priority task. Consider doing it when convenient.")
+
+    case "low":
+        if time_bound == "yes":
+            print(f"Reminder: '{task}' is a {priority} priority task that you may want to finish today.")
+        else:
+            print(f"Reminder: '{task}' is a {priority} priority task. Consider completing it when you have free time.")
+
+    case _:
+        print("Reminder: Invalid priority entered. Please use high, medium, or low.")
