4180b956c47528356997f42323c723c6069daf8b
Wilfred Osozi <osoziw@gmail.com>
2025-12-12 08:40:55 +0300

    updated

--- oop/book_class.py
+++ oop/book_class.py
@@ -7,8 +7,8 @@ class Book:
         return f"{self.title} by {self.author}, published in {self.year}"
 
     def __repr__(self):
-        return f"Book({self.title}, {self.author}, {self.year})"
+        return f"Book('{self.title}', '{self.author}', '{self.year}')"
     
     def __del__(self):
-        print(f"Deleting {self.title} by {self.author}")
+        print(f"Deleting {self.title}")
 
